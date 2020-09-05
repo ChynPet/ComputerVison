3033aecb398dcb768a6608290e485d1b95060675
Chynnyk Petro <chynnyk@vivaldi.net>
2020-09-05 14:05:24 +0300

    Revert "Revert "Created files main.py and additional.py. In file main.py main task. In file additional.py bonus task. Version homework 0.1.0""
    This reverts commit 3dadf76f33786ffc934ef4b216533742fe1d99de.
    
    	new file:   lab1/.gitignore
    	new file:   lab1/.idea/.gitignore
    	new file:   lab1/.idea/inspectionProfiles/profiles_settings.xml
    	new file:   lab1/.idea/lab1.iml
    	new file:   lab1/.idea/misc.xml
    	new file:   lab1/.idea/modules.xml
    	new file:   lab1/.idea/other.xml
    	new file:   lab1/.idea/vcs.xml
    	new file:   lab1/additional.py
    	new file:   lab1/main.py

---
+++ lab1/main.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import logging
+import numpy as np
+
+LOGFILE = 'camera.log'
+logging.basicConfig(filename=LOGFILE, level='DEBUG')
+
+webcam = cv2.VideoCapture(0)
+img_count = 0
+
+while True:
+
+    # if webcam is opened
+    if not webcam.isOpened():
+        logging.warning('Unable to connect to camera.')
+        time.sleep(5)
+        continue
+
+    # get image from camera
+    ret, frame = webcam.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        logging.warning("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # show image
+    cv2.imshow('Press ESC - for quite. SPACE - for snaphot', frame)
+
+    # key for snaphot or quit
+    k = cv2.waitKey(1)
+    if k % 256 == 27:
+        # ESC pressed for quit
+        logging.info('Escape hit, closing...')
+        break
+    elif k % 256 == 32:
+        # SPACE pressed for save
+        img_name = f'opencv_frame_{img_count}.png'
+        cv2.imwrite(img_name, frame)
+        logging.info(f'{img_name} written!')
+
+        # read image
+        img = cv2.imread(img_name, 1)
+
+        # get width and height for points rectangle and line
+        height, width = img.shape[:2]
+        # upper left point
+        A1_up_left = (int(width / 4), int(height / 4))
+        # downer right point
+        A2_down_right = (int(3 * width / 4), int(3 * height / 4))
+
+        # convert image to grayscale
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        colors_rectangle = (0, 255, 0)
+        colors_line = (255, 0, 0)
+        thickness = 5
+
+        # draw rectangle on image
+        cv2.rectangle(gray_img, A1_up_left, A2_down_right, colors_rectangle, thickness)
+        # draw line on image
+        cv2.line(gray_img, A1_up_left, A2_down_right, colors_line, thickness)
+
+        cv2.imshow(img_name, gray_img)
+        img_count += 1
+
+# close everything
+webcam.release()
+cv2.destroyAllWindows()
